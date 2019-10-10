dd93f109555dcc5cfae0437048a004cbd9a57aac
rangdl <378694192@qq.com>
2019-10-10 19:04:28 +0800

    re2

---
+++ common-dao/src/main/java/com/example/common/mapper/auth/RoleAuthorityMapper.java
@@ -0,0 +1,37 @@
+package com.example.common.mapper.auth;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.common.pojo.entity.auth.RoleAuthority;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @author rdl
+ * @version V1.0
+ * @ClassName RoleAuthorityMapper
+ * @Description TODO 权限表 Mapper 接口
+ * @date 2019/10/9 18:49
+ **/
+public interface RoleAuthorityMapper extends BaseMapper<RoleAuthority> {
+    /**
+     * 根据角色Id删除
+     * @param role
+     * @return
+     */
+    int deleteAuthByRoleId(RoleAuthority role);
+
+    /**
+     * 批量插入
+     * @param roleAuthList
+     */
+    void batchInsert(List<RoleAuthority> roleAuthList);
+
+    /**
+     * 根据角色Id获取权限
+     * @param roleId
+     * @return
+     */
+    List<Long> selectAuthByRoleId(@Param(value = "roleId") Long roleId);
+
+}
